95524cc13e5bdfa69d26bdd62c3f33e69b078ab0
Modestas <modestas.kazlauskas@ktu.edu>
2020-06-14 12:35:22 +0300

    Update Sum.cs

--- Code Snippets/LINQ/Sum.cs
+++ Code Snippets/LINQ/Sum.cs
@@ -12,7 +12,8 @@ double totalWeight = packages.Sum(pkg => pkg.Weight);
 
 // Example 2
 
-var players = new List<Player> {
+var players = new List<Player>
+{
     new Player { Name = "Alex", Team = "A", Score = 10 },
     new Player { Name = "Anna", Team = "A", Score = 20 },
     new Player { Name = "Luke", Team = "L", Score = 60 },
@@ -21,12 +22,12 @@ var players = new List<Player> {
 
 var teamTotalScores =
     from player in players
-    group player by player.Team into playerGroup
-    select new
-    {
-        Team = playerGroup.Key,
-        TotalScore = playerGroup.Sum(x => x.Score),
-    };
+group player by player.Team into playerGroup
+select new
+{
+    Team = playerGroup.Key,
+    TotalScore = playerGroup.Sum(x => x.Score),
+};
 
 // Results:
 // { Team = "A", TotalScore = 30 }
@@ -41,25 +42,27 @@ int sum = (from x in list where x > 4 select x).Sum();
 
 // Example 4
 
-IList<Student> studentList = new List<Student>>() { 
-        new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
-        new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
-        new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
-        new Student() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
-        new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 } 
-    };
+IList<Student> studentList = new List<Student>>()
+{
+    new Student() { StudentID = 1, StudentName = "John", Age = 13 },
+    new Student() { StudentID = 2, StudentName = "Moin", Age = 21 },
+    new Student() { StudentID = 3, StudentName = "Bill", Age = 18 },
+    new Student() { StudentID = 4, StudentName = "Ram", Age = 20 },
+    new Student() { StudentID = 5, StudentName = "Ron", Age = 15 }
+};
 
 var sumOfAge = studentList.Sum(s => s.Age);
-		
-var numOfAdults = studentList.Sum(s => {
-			
-	if(s.Age >= 18)
-	    return 1;
-	else
-	    return
 
- 0;
+var numOfAdults = studentList.Sum(s =>
+{
+
+    if (s.Age >= 18)
+        return 1;
+    else
+        return
+
+    0;
 });
 
 // Total Age of Student: 87
-// Total Adult Students: 3 
+// Total Adult Students: 3
